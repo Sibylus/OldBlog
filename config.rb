a3a21090b5942b974854516020e0344eab0e59bd
Matt Capers <mcapers@gmail.com>
2013-02-22 15:48:42 -0800

    Fixed asset caching and images path

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ project_type = :stand_alone
 
 # Publishing paths
 http_path = "/blog/"
-http_images_path = ENV['OCTOPRESS_ENV'] == "preview" ? "/blog/images" : "http://oblog.github.com/images"
+http_images_path = ENV['OCTOPRESS_ENV'] == "preview" ? "/blog/images" : "http://oblog.rentobo.com/images"
 http_fonts_path = "/blog/fonts"
 css_dir = "public/blog/stylesheets"
 
@@ -12,6 +12,7 @@ css_dir = "public/blog/stylesheets"
 sass_dir = "sass"
 images_dir = "source/images"
 fonts_dir = "source/fonts"
+asset_cache_buster :none
 
 line_comments = false
 output_style = :compressed
